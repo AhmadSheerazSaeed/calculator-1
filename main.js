d7475dbeb30724499f43a6ab667da2136731722c
AhmadSheerazSaeed <khera724@gmail.com>
2022-10-25 23:41:32 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,98 @@
+"use strict";
+
+let num1 = 0;
+let num2 = 0;
+let resultOfCalculations = 0;
+let operatorForCalculations = "";
+
+const inputResult = document.querySelector("#result");
+const inputValue = document.querySelector("#inputValue");
+const buttonsElements = document.querySelectorAll(".digitButtons");
+const calcButtonsElements = document.querySelectorAll(".calcButtons");
+const equalButton = document.querySelector(".equalButton");
+const resetButton = document.querySelector(".resetButton");
+
+// console.log(buttonsElements);
+
+for (let count = 0; count < buttonsElements.length; count++) {
+  // console.log(buttonsElements[count]);
+  buttonsElements[count].addEventListener("click", () => {
+    // console.log(buttonsElements[count].textContent);
+    inputValue.value += buttonsElements[count].textContent;
+  });
+}
+
+for (let count = 0; count < calcButtonsElements.length; count++) {
+  // console.log(calcButtonsElements[count].textContent);
+  calcButtonsElements[count].addEventListener("click", () => {
+    operatorForCalculations = calcButtonsElements[count].textContent;
+    num1 = inputValue.value;
+    inputValue.value = "";
+    // console.log("num1" + num1);
+
+    // if (inputValue.value !== "" && num1 === 0) {
+    //   num1 = inputValue.value;
+    //   console.log("num1 : " + num1);
+    //   inputValue.value = "";
+    // } else if (inputValue.value !== "" && (num1 > 0 || num1 < 0)) {
+    //   num2 = inputValue.value;
+    //   console.log("num 2 : " + num2);
+    //   inputValue.value = "";
+    // }
+  });
+}
+
+equalButton.addEventListener("click", () => {
+  num2 = inputValue.value;
+  inputValue.value = "";
+  // console.log("num2 " + num2);
+
+  // console.log("num1 : " + num1);
+  // console.log("num2 : " + num2);
+  // console.log("operator : " + operatorForCalculations);
+  calculationOfNumbers(num1, num2, operatorForCalculations);
+});
+
+function calculationOfNumbers(num1 = 0, num2 = 0, calcOperator) {
+  // console.log("num1 : " + num1);
+  // console.log("num2 : " + num2);
+  // console.log("operator : " + calcOperator);
+
+  switch (calcOperator) {
+    case "+":
+      // console.log("+");
+      inputResult.value = `${Number(num1)} + ${Number(num2)} = ${
+        Number(num1) + Number(num2)
+      }`;
+      // resultOfCalculations = num1 + num2;
+      break;
+    case "-":
+      // console.log("-");
+      inputResult.value = `${Number(num1)} - ${Number(num2)} = ${
+        Number(num1) - Number(num2)
+      }`;
+      break;
+    case "*":
+      // console.log("*");
+      inputResult.value = `${Number(num1)} * ${Number(num2)} = ${
+        Number(num1) * Number(num2)
+      }`;
+      break;
+    case "/":
+      // console.log("/");
+      inputResult.value = `${Number(num1)} / ${Number(num2)} = ${
+        Number(num1) / Number(num2)
+      }`;
+      break;
+    default:
+      inputResult.value = "";
+  }
+}
+
+resetButton.addEventListener("click", () => {
+  num1 = 0;
+  num2 = 0;
+  operatorForCalculations = "";
+  inputValue.value = "";
+  inputResult.value = "";
+});
