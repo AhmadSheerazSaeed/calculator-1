5a2096143a947d0267b170db48ce7c2592f5e209
AhmadSheerazSaeed <khera724@gmail.com>
2022-10-27 22:47:33 +0200

    code for numeric keypad add

--- main.js
+++ main.js
@@ -12,23 +12,76 @@ const calcButtonsElements = document.querySelectorAll(".calcButtons");
 const equalButton = document.querySelector(".equalButton");
 const resetButton = document.querySelector(".resetButton");
 
-// get value from the pressed button
+// get value from the digit buttons pressed
 for (let count = 0; count < buttonsElements.length; count++) {
   buttonsElements[count].addEventListener("click", () => {
-    inputValue.value += buttonsElements[count].textContent;
+    // inputValue.value += buttonsElements[count].textContent;
+    getDigits(buttonsElements[count].textContent);
   });
 }
 
-// get airthmetic operators on button pressed
+// input from keyboard
+document.addEventListener("keydown", (event) => {
+  // console.log(event);
+  if (
+    event.key === "1" ||
+    event.key === "2" ||
+    event.key === "3" ||
+    event.key === "4" ||
+    event.key === "5" ||
+    event.key === "6" ||
+    event.key === "7" ||
+    event.key === "8" ||
+    event.key === "9" ||
+    event.key === "0"
+  ) {
+    getDigits(event.key);
+  } else if (event.key === "," || event.key === ".") {
+    // console.log(inputValue.value.indexOf("."));
+    getDigits(".");
+  }
+
+  // get arithmetic operator
+  if (
+    event.key === "+" ||
+    event.key === "-" ||
+    event.key === "*" ||
+    event.key === "/"
+  ) {
+    getArithmeticOperator(event.key);
+  }
+
+  if (event.key === "Enter") {
+    calcValues();
+  }
+});
+
+// function to display input in text box
+function getDigits(digitValue) {
+  inputValue.value += digitValue;
+}
+
+// get arithmetic operators on button pressed
 for (let count = 0; count < calcButtonsElements.length; count++) {
   calcButtonsElements[count].addEventListener("click", () => {
     operatorForCalculations = calcButtonsElements[count].textContent;
-    inputValue.value += operatorForCalculations;
+    // inputValue.value += operatorForCalculations;
+
+    getArithmeticOperator(operatorForCalculations);
   });
 }
 
+// function to display arithmetic operator in input field
+function getArithmeticOperator(arithmeticOperator) {
+  operatorForCalculations = arithmeticOperator;
+  inputValue.value += arithmeticOperator;
+}
+
 // show result after pressing the equal to button
-equalButton.addEventListener("click", () => {
+equalButton.addEventListener("click", calcValues);
+
+// function to get values and send these values for calculation
+function calcValues() {
   // num2 = inputValue.value;
   num1 = inputValue.value.substring(
     0,
@@ -40,9 +93,9 @@ equalButton.addEventListener("click", () => {
   );
   calculationOfNumbers(num1, num2, operatorForCalculations);
   inputValue.value = "";
-});
+}
 
-// function to display result of input
+// function to calculate result of input
 function calculationOfNumbers(num1 = 0, num2 = 0, calcOperator = "+") {
   switch (calcOperator) {
     case "+":
